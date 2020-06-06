6d0e54134f753ee37116af3906ef99b0fc3f7864
cassandreh1 <cassandreh01@gmail.com>
2020-06-06 12:22:10 -0400

    displaying data from beer api in list component

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-list',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
-
-  constructor() { }
+  brews: Object;
+  // dependency injection
+  constructor(private _http: HttpService) { }
 
   ngOnInit(): void {
+    // return an obversable therefore you can subscribe to it
+    this._http.getBeer().subscribe(data => {
+      console.log(data);
+      this.brews = data;
+    })
   }
 
 }
